ca0db7bfa828c42e9ec2c0dc88f80ddbf28a046e
Roman Mühlfeldner <roman@muehlfeldner.net>
2020-02-20 18:11:47 +0100

    Updates

---
+++ static/js/4.82c13fc6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpionic-react-boilerplate"]=this["webpackJsonpionic-react-boilerplate"]||[]).push([[4],{185:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(32),o=(e(5),function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.g)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.l)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=4.82c13fc6.chunk.js.map
